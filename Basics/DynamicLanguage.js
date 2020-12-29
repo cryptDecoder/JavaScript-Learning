52760cb4232d7a0e0934229cd3829da21579d8f1
cryptDecoder <pruthvirajs2007@gmail.com>
2020-12-29 11:03:14 +0530

    Dynamic Language

---
+++ Basics/DynamicLanguage.js
@@ -0,0 +1,9 @@
+let value = "Jake";
+console.log("====================================");
+console.log(typeof value);
+console.log("====================================");
+
+value = 10;
+console.log("====================================");
+console.log(typeof value);
+console.log("====================================");
