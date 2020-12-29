d8a03ae5bb1751c943827c0efd2eb99614fdb7fd
cryptDecoder <pruthvirajs2007@gmail.com>
2020-12-29 10:53:39 +0530

    js variables

---
+++ Basics/variables.js
@@ -0,0 +1,24 @@
+// use of variables to store the value temporary and use over the program
+
+let name = "Jake";
+console.log(name);
+
+// Rules for variable declaration
+
+// 1. Can not be reserved keyword.
+// 2. should be meaningful
+// 3. cannot  start with a number
+// 4. cannot contain a space or hyphen
+// 5. Are case-sensitive
+
+// Constants
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+// value of variable change but constant not be changes.
+
+const rate = 12;
+//rate = 1233;
+console.log(rate);
