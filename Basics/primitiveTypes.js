2ad2855d470554ac4d1b9865d77d5f40e89b697c
cryptDecoder <pruthvirajs2007@gmail.com>
2020-12-29 10:59:18 +0530

    Types in js

---
+++ Basics/primitiveTypes.js
@@ -0,0 +1,22 @@
+// primitive/ Value Types
+/*
+    1. String
+    2. Number
+    3. Boolean
+    4. Undefined 
+    5. null
+
+
+*/
+
+let name = "Jake"; // String literal
+let age = 20; // number literal
+let isApproved = true; // boolean literal
+let firstName = undefined; // undefined
+let lastName = null; // null literal
+
+console.log("====================================");
+console.log(name, age, isApproved, firstName, lastName);
+console.log("====================================");
+
+
