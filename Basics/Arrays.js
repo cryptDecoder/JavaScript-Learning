c76684bdc842cc6be3b8a42f464e3f8dc59f8ea0
cryptDecoder <pruthvirajs2007@gmail.com>
2020-12-31 16:48:02 +0530

    Array's in javascript

---
+++ Basics/Arrays.js
@@ -0,0 +1,21 @@
+// Array in javascript
+
+let selectedColors = ["red", "Blue"];
+
+console.log("====================================");
+console.log(selectedColors);
+console.log("====================================");
+
+//  add element in array
+selectedColors[2] = "green";
+console.log("====================================");
+console.log(selectedColors);
+console.log("====================================");
+
+// index in arrays
+
+console.log("====================================");
+console.log(selectedColors[0]);
+console.log("====================================");
+
+// array is an object in javascript
