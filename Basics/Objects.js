a397f29dab319e2f45ec6afb3f9e6d54b9bf5b45
cryptDecoder <pruthvirajs2007@gmail.com>
2020-12-31 16:43:29 +0530

    Object Notation

---
+++ Basics/Objects.js
@@ -0,0 +1,23 @@
+//create object
+
+const person = {
+  name: "Mosh",
+  age: 23,
+};
+
+console.log("====================================");
+console.log(person);
+console.log("====================================");
+
+// update the person
+person.name = "John";
+console.log("====================================");
+console.log(person.name);
+console.log("====================================");
+
+// brackets notation
+
+person["name"] = "merry";
+console.log("====================================");
+console.log(person.name);
+console.log("====================================");
